7e853b29caf12744e1a630d79664889f839db9dc
Antonio Dell'Elce <antonio.dellelce@thomsonreuters.com>
2014-05-13 13:08:42 +0100

    initial

---
+++ com/caucho/quercus/php/FilterIterator.php
@@ -0,0 +1,34 @@
+<?php
+
+abstract class FilterIterator extends IteratorIterator
+{
+  function __construct($iter)
+  {
+    parent::__construct($iter);
+  }
+  
+  abstract function accept();
+
+  function fetch()
+  {
+    for (; $this->it->valid() && ! $this->accept(); $this->it->next()) {
+    }
+  }
+
+  function next()
+  {
+    parent::next();
+    $this->fetch();
+  }    
+
+  function rewind()
+  {
+    parent::rewind();
+    $this->fetch();
+  }
+
+  function __call($fun, $param)
+  {
+    return call_user_func_array(array($this->it, $fun), $param);
+  }
+}
